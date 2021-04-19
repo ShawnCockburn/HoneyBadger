fbdc16131d8dee440a4c74937b488d83849f8564
ShawnCockburn <shawncockburn@hotmail.com>
2021-04-19 19:30:10 +0100

    add: Matirial UI

---
+++ src/Components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Typography } from "@material-ui/core";
+
+const App = (props) => {
+  return (
+    <div>
+      <Typography variant="h3">
+      HoneyBadger
+      </Typography>
+    </div>
+  );
+};
+
+export default App;
